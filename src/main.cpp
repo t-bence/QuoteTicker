8ca9af7bb64d5a57fa763b0683a564bf7d461752
t-bence <toth.bence.mihaly@gmail.com>
2020-03-20 22:37:57 +0100

    webpage works, can Serial.print POSTed message

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,114 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+#include <MaxMatrix.h>
+#include <avr/pgmspace.h>
+PROGMEM const unsigned char CH[] = {
+  3, 8, B00000000, B00000000, B00000000, B00000000, B00000000, // space
+  1, 8, B01011111, B00000000, B00000000, B00000000, B00000000, // !
+  3, 8, B00000011, B00000000, B00000011, B00000000, B00000000, // "
+  5, 8, B00010100, B00111110, B00010100, B00111110, B00010100, // #
+  4, 8, B00100100, B01101010, B00101011, B00010010, B00000000, // $
+  5, 8, B01100011, B00010011, B00001000, B01100100, B01100011, // %
+  5, 8, B00110110, B01001001, B01010110, B00100000, B01010000, // &
+  1, 8, B00000011, B00000000, B00000000, B00000000, B00000000, // '
+  3, 8, B00011100, B00100010, B01000001, B00000000, B00000000, // (
+  3, 8, B01000001, B00100010, B00011100, B00000000, B00000000, // )
+  5, 8, B00101000, B00011000, B00001110, B00011000, B00101000, // *
+  5, 8, B00001000, B00001000, B00111110, B00001000, B00001000, // +
+  2, 8, B10110000, B01110000, B00000000, B00000000, B00000000, // ,
+  4, 8, B00001000, B00001000, B00001000, B00001000, B00000000, // -
+  2, 8, B01100000, B01100000, B00000000, B00000000, B00000000, // .
+  4, 8, B01100000, B00011000, B00000110, B00000001, B00000000, // /
+  4, 8, B00111110, B01000001, B01000001, B00111110, B00000000, // 0
+  3, 8, B01000010, B01111111, B01000000, B00000000, B00000000, // 1
+  4, 8, B01100010, B01010001, B01001001, B01000110, B00000000, // 2
+  4, 8, B00100010, B01000001, B01001001, B00110110, B00000000, // 3
+  4, 8, B00011000, B00010100, B00010010, B01111111, B00000000, // 4
+  4, 8, B00100111, B01000101, B01000101, B00111001, B00000000, // 5
+  4, 8, B00111110, B01001001, B01001001, B00110000, B00000000, // 6
+  4, 8, B01100001, B00010001, B00001001, B00000111, B00000000, // 7
+  4, 8, B00110110, B01001001, B01001001, B00110110, B00000000, // 8
+  4, 8, B00000110, B01001001, B01001001, B00111110, B00000000, // 9
+  2, 8, B01010000, B00000000, B00000000, B00000000, B00000000, // :
+  2, 8, B10000000, B01010000, B00000000, B00000000, B00000000, // ;
+  3, 8, B00010000, B00101000, B01000100, B00000000, B00000000, // <
+  3, 8, B00010100, B00010100, B00010100, B00000000, B00000000, // =
+  3, 8, B01000100, B00101000, B00010000, B00000000, B00000000, // >
+  4, 8, B00000010, B01011001, B00001001, B00000110, B00000000, // ?
+  5, 8, B00111110, B01001001, B01010101, B01011101, B00001110, // @
+  4, 8, B01111110, B00010001, B00010001, B01111110, B00000000, // A
+  4, 8, B01111111, B01001001, B01001001, B00110110, B00000000, // B
+  4, 8, B00111110, B01000001, B01000001, B00100010, B00000000, // C
+  4, 8, B01111111, B01000001, B01000001, B00111110, B00000000, // D
+  4, 8, B01111111, B01001001, B01001001, B01000001, B00000000, // E
+  4, 8, B01111111, B00001001, B00001001, B00000001, B00000000, // F
+  4, 8, B00111110, B01000001, B01001001, B01111010, B00000000, // G
+  4, 8, B01111111, B00001000, B00001000, B01111111, B00000000, // H
+  3, 8, B01000001, B01111111, B01000001, B00000000, B00000000, // I
+  4, 8, B00110000, B01000000, B01000001, B00111111, B00000000, // J
+  4, 8, B01111111, B00001000, B00010100, B01100011, B00000000, // K
+  4, 8, B01111111, B01000000, B01000000, B01000000, B00000000, // L
+  5, 8, B01111111, B00000010, B00001100, B00000010, B01111111, // M
+  5, 8, B01111111, B00000100, B00001000, B00010000, B01111111, // N
+  4, 8, B00111110, B01000001, B01000001, B00111110, B00000000, // O
+  4, 8, B01111111, B00001001, B00001001, B00000110, B00000000, // P
+  4, 8, B00111110, B01000001, B01000001, B10111110, B00000000, // Q
+  4, 8, B01111111, B00001001, B00001001, B01110110, B00000000, // R
+  4, 8, B01000110, B01001001, B01001001, B00110010, B00000000, // S
+  5, 8, B00000001, B00000001, B01111111, B00000001, B00000001, // T
+  4, 8, B00111111, B01000000, B01000000, B00111111, B00000000, // U
+  5, 8, B00001111, B00110000, B01000000, B00110000, B00001111, // V
+  5, 8, B00111111, B01000000, B00111000, B01000000, B00111111, // W
+  5, 8, B01100011, B00010100, B00001000, B00010100, B01100011, // X
+  5, 8, B00000111, B00001000, B01110000, B00001000, B00000111, // Y
+  4, 8, B01100001, B01010001, B01001001, B01000111, B00000000, // Z
+  2, 8, B01111111, B01000001, B00000000, B00000000, B00000000, // [
+  4, 8, B00000001, B00000110, B00011000, B01100000, B00000000, // \ backslash
+  2, 8, B01000001, B01111111, B00000000, B00000000, B00000000, // ]
+  3, 8, B00000010, B00000001, B00000010, B00000000, B00000000, // hat
+  4, 8, B01000000, B01000000, B01000000, B01000000, B00000000, // _
+  2, 8, B00000001, B00000010, B00000000, B00000000, B00000000, // `
+  4, 8, B00100000, B01010100, B01010100, B01111000, B00000000, // a
+  4, 8, B01111111, B01000100, B01000100, B00111000, B00000000, // b
+  4, 8, B00111000, B01000100, B01000100, B00101000, B00000000, // c
+  4, 8, B00111000, B01000100, B01000100, B01111111, B00000000, // d
+  4, 8, B00111000, B01010100, B01010100, B00011000, B00000000, // e
+  3, 8, B00000100, B01111110, B00000101, B00000000, B00000000, // f
+  4, 8, B10011000, B10100100, B10100100, B01111000, B00000000, // g
+  4, 8, B01111111, B00000100, B00000100, B01111000, B00000000, // h
+  3, 8, B01000100, B01111101, B01000000, B00000000, B00000000, // i
+  4, 8, B01000000, B10000000, B10000100, B01111101, B00000000, // j
+  4, 8, B01111111, B00010000, B00101000, B01000100, B00000000, // k
+  3, 8, B01000001, B01111111, B01000000, B00000000, B00000000, // l
+  5, 8, B01111100, B00000100, B01111100, B00000100, B01111000, // m
+  4, 8, B01111100, B00000100, B00000100, B01111000, B00000000, // n
+  4, 8, B00111000, B01000100, B01000100, B00111000, B00000000, // o
+  4, 8, B11111100, B00100100, B00100100, B00011000, B00000000, // p
+  4, 8, B00011000, B00100100, B00100100, B11111100, B00000000, // q
+  4, 8, B01111100, B00001000, B00000100, B00000100, B00000000, // r
+  4, 8, B01001000, B01010100, B01010100, B00100100, B00000000, // s
+  3, 8, B00000100, B00111111, B01000100, B00000000, B00000000, // t
+  4, 8, B00111100, B01000000, B01000000, B01111100, B00000000, // u
+  5, 8, B00011100, B00100000, B01000000, B00100000, B00011100, // v
+  5, 8, B00111100, B01000000, B00111100, B01000000, B00111100, // w
+  5, 8, B01000100, B00101000, B00010000, B00101000, B01000100, // x
+  4, 8, B10011100, B10100000, B10100000, B01111100, B00000000, // y
+  3, 8, B01100100, B01010100, B01001100, B00000000, B00000000, // z
+  3, 8, B00001000, B00110110, B01000001, B00000000, B00000000, // {
+  1, 8, B01111111, B00000000, B00000000, B00000000, B00000000, // |
+  3, 8, B01000001, B00110110, B00001000, B00000000, B00000000, // }
+  4, 8, B00001000, B00000100, B00001000, B00000100, B00000000, // ~
+};
+int DIN = 7;   // DIN pin of MAX7219 module
+int CLK = 6;   // CLK pin of MAX7219 module
+int CS = 5;    // CS pin of MAX7219 module
+int maxInUse = 2;
+MaxMatrix m(DIN, CS, CLK, maxInUse);
+byte buffer[10];
+
 /* Put your SSID & Password */
-const char* ssid = "NodeMCU";  // Enter SSID here
+const char* ssid = "QuoteTicker";  // Enter SSID here
 const char* password = "12345678";  //Enter Password here
 
 /* Put IP Address details */
@@ -13,41 +119,87 @@ IPAddress subnet(255,255,255,0);
 
 ESP8266WebServer server(80);
 
+// char messages[1024] {"Hello world!"};
+
+String messages {"Hello vilag"};
+
+String html_start {"<html><head><style>.u-full-width { width: 100%; box-sizing: border-box; } .button, button, input { display: block; height: 38px; padding: 0 30px; color: #FFF; text-align: center; font-size: 11px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: #33C3F0; border-radius: 4px; border: 1px solid #33C3F0; cursor: pointer; box-sizing: border-box; } textarea { height: 38px; background-color: #fff; border: 1px solid #D1D1D1; border-radius: 4px; box-shadow: none; box-sizing: border-box; } textarea { -webkit-appearance: none; -moz-appearance: none; appearance: none; } textarea { min-height: 65px; padding-top: 6px; padding-bottom: 6px; } textarea:focus { border: 1px solid #33C3F0; outline: 0; }</style><title>QuoteTicker</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\"><script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script></head><body><h1>Add meg az id&eacute;zetet</h1><form action=\"/\" method=\"POST\"><textarea class=\"u-full-width\" id=\"exampleMessage\" name=\"message\">"};
+String html_end {"</textarea><input class=\"button-primary\" type=\"submit\" value=\"K&uuml;ld&eacute;s!\"></form></body></html>"};
+
+void showPage();
+void handlePost();
+void handle404();
+void printMessage();
+void printCharWithShift(char c, int shift_speed);
+void printStringWithShift(char* s, int shift_speed);
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
   
-  server.on("/", handle_OnConnect);
-  server.on("/led1on", handle_led1on);
-  server.onNotFound(handle_NotFound);
+  server.on("/", HTTP_GET, showPage);
+  server.on("/", HTTP_POST, handlePost);
+  server.onNotFound(handle404);
   
   server.begin();
   Serial.println("HTTP server started");
+
+  // set up LED display
 }
+
 void loop() {
   server.handleClient();
+}
 
-  // do application logic
-  // read new text
+void showPage() {
+  // display current settings on webpage
+  Serial.println("Showing page");
+  server.send(200, "text/html", html_start + messages + html_end);
+
+  printMessage();
+}
+
+void handlePost() {
+  // save & display
+  // get text
+  Serial.println("POST request: ");
+  messages = server.arg(0);
+  Serial.println(messages);
+  
   // save to EEPROM
-  // write out
+
+  showPage();
 }
 
-void handle_OnConnect() {
-
-  Serial.println("GPIO7 Status: OFF | GPIO6 Status: OFF");
-  server.send(200, "text/html", "HTML text comes here"); 
-}
-
-void handle_led1on() {
-  Serial.println("GPIO7 Status: ON");
-  server.send(200, "text/html", "HTML comes here too"); 
-}
-
-void handle_NotFound(){
+void handle404() {
   server.send(404, "text/plain", "Not found");
+}
+
+void printMessage() {
+  // display message on the LEDs
+  // printStringWithShift(messages, 100);
+}
+
+void printCharWithShift(char c, int shift_speed) {
+  if (c < 32) return;
+  c -= 32;
+  memcpy_P(buffer, CH + 7 * c, 7);
+  m.writeSprite(32, 0, buffer);
+  m.setColumn(32 + buffer[0], 0);
+  for (int i = 0; i < buffer[0] + 1; i++)
+  {
+    delay(shift_speed);
+    m.shiftLeft(false, false);
+  }
+}
+// Extract the characters from the text string
+void printStringWithShift(char* s, int shift_speed) {
+  while (*s != 0) {
+    printCharWithShift(*s, shift_speed);
+    s++;
+  }
 }
