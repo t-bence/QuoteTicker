c3f3f592a7b373b4832ee1d36eba4f9ed2901c98
t-bence <toth.bence.mihaly@gmail.com>
2020-03-21 21:40:43 +0100

    works but with flicker

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # QuoteTicker
 
+## Description
 An Arduino program to display some text on LED boards. The text can be set using a WiFi access point.
 
 The code uses some elements of [this](https://lastminuteengineers.com/creating-esp8266-web-server-arduino-ide/] and [this](https://howtomechatronics.com/tutorials/arduino/8x8-led-matrix-max7219-tutorial-scrolling-text-android-control-via-bluetooth/) tutorial.  
 [markruys' MAX7219 library](https://github.com/markruys/arduino-Max72xxPanel) library is used to drive the LED matrix together with [Adafruit's GFX library](https://github.com/adafruit/Adafruit-GFX-Library).
 Parts of the [Skeleton CSS toolkit](http://getskeleton.com/) are used, as well.
 
-# TODO
+## Pinout
+According to [Hackster.io](https://www.hackster.io/FilippoOnesti/esp8266-clock-using-max7219-led-matrix-display-b036c7), the pinout for a Wemos ESP8266 is the following:
+```
+LED Matrix Pin -> ESP8266 Pin
+Vcc            -> 3v  (3V on NodeMCU 3V3 on WEMOS)
+Gnd            -> Gnd (G on NodeMCU)
+DIN            -> D7  (Same Pin for WEMOS)
+CS             -> D4  (Same Pin for WEMOS)
+CLK            -> D5  (Same Pin for WEMOS)
+```
+
+## TODO
 - [x] Make it read text submitted via a `textarea`.
-- [ ] Make it display and scroll the text.
-- [ ] Make it store the text when switched off.
-- [ ] Store strings somewhere.
+- [x] Make it display and scroll the text.
+- [ ] Make it store the text when switched off: SPIFFS?
+- [ ] Reduce flicker
+
+## Ideas, notes
+* I could store the strings in the SPIFFS: descriptions [here](https://techtutorialsx.com/2018/08/05/esp32-arduino-spiffs-reading-a-file/) and [here](https://techtutorialsx.com/2019/05/28/esp8266-spiffs-writing-a-file/).
+
+* Rotation introduces flicker. When using Adafruit's original `setRotation`, exposed by `Max72xxPanel::setRotation`, there is no flicker but the display won't work properly (one is upside down, the other is not, and there is no was to specify rotation on a per-display basis). So I have to look and combine the approaches from the two libraries.
--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,15 @@
 const char* ssid = "QuoteTicker";
 const char* password = "12345678";
 
-int pinCS = 10; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+int pinCS = D4; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
 int numberOfHorizontalDisplays = 4;
 int numberOfVerticalDisplays = 1;
 
 Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 
-int wait = 20; // In milliseconds
-uint8_t spacer = 1;
-uint8_t width = 5 + spacer; // The font width is 5 pixels
+int wait = 40; // In milliseconds
+uint16_t spacer = 1;
+uint16_t width = 5 + spacer; // The font width is 5 pixels
 
 // configurations over
 
@@ -32,7 +32,7 @@ ESP8266WebServer server(80);
 String messages {"Hello vilag"};
 String messagesOneline {""};
 
-String html_start {"<html><head><style>.u-full-width { width: 100%; box-sizing: border-box; } .button, button, input { display: block; height: 38px; padding: 0 30px; color: #FFF; text-align: center; font-size: 11px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: #33C3F0; border-radius: 4px; border: 1px solid #33C3F0; cursor: pointer; box-sizing: border-box; } textarea { background-color: #fff; border: 1px solid #D1D1D1; border-radius: 4px; box-shadow: none; box-sizing: border-box; } textarea { -webkit-appearance: none; -moz-appearance: none; appearance: none; } textarea { min-height: 65px; padding-top: 6px; padding-bottom: 6px; } textarea:focus { border: 1px solid #33C3F0; outline: 0; }</style><title>QuoteTicker</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\"><script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script></head><body><h1>Add meg az id&eacute;zetet</h1><form action=\"/\" method=\"POST\"><textarea class=\"u-full-width\" id=\"exampleMessage\" name=\"message\" rows=6>"};
+String html_start {"<html><head><style>h1 {font-size: 40px; } .u-full-width { width: 100%; box-sizing: border-box; } .button, button, input { display: block; height: 100px; padding: 0 30px; color: #FFF; text-align: center; font-size: 30px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: #33C3F0; border-radius: 4px; border: 1px solid #33C3F0; cursor: pointer; box-sizing: border-box; } textarea { font-size: 38px; background-color: #fff; border: 1px solid #D1D1D1; border-radius: 4px; box-shadow: none; box-sizing: border-box; } textarea { -webkit-appearance: none; -moz-appearance: none; appearance: none; } textarea { min-height: 65px; padding-top: 6px; padding-bottom: 6px; } textarea:focus { border: 1px solid #33C3F0; outline: 0; }</style><title>QuoteTicker</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\"><script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script></head><body><h1>Add meg az id&eacute;zetet</h1><form action=\"/\" method=\"POST\"><textarea class=\"u-full-width\" id=\"exampleMessage\" name=\"message\" rows=6>"};
 String html_end {"</textarea><input class=\"button-primary\" type=\"submit\" value=\"K&uuml;ld&eacute;s!\"></form></body></html>"};
 
 void showPage();
@@ -42,7 +42,7 @@ void printMessage();
 
 void setup() {
   messagesOneline = messages;
-  messagesOneline.replace("\n", "   ");
+  messagesOneline.replace("\r\n", "   ");
   Serial.begin(9600);
 
   WiFi.softAP(ssid, password);
@@ -57,33 +57,39 @@ void setup() {
   Serial.println("HTTP server started");
 
   // set up LED display
-  matrix.setIntensity(7);
+  matrix.setIntensity(3);
+  
+  matrix.setRotation(0, 1);
+  matrix.setRotation(1, 1);
+  matrix.setRotation(2, 1);
+  matrix.setRotation(3, 1);
+
 }
 
 void loop() {
-  server.handleClient();
   // display message on the LEDs
-  for ( uint16_t i = 0 ; i < width * messagesOneline.length() + matrix.width() - 1 - spacer; i++ ) {
+    for ( uint16_t i = 0 ; i < width * messagesOneline.length() + matrix.width() - 1 - spacer; i++ ) {
+      server.handleClient();
 
-    matrix.fillScreen(LOW);
+      matrix.fillScreen(LOW);
 
-    uint16_t letter = i / width;
-    int x = (matrix.width() - 1) - i % width;
-    int y = (matrix.height() - 8) / 2; // center the text vertically
+      uint16_t letter = i / width;
+      int x = (matrix.width() - 1) - i % width;
+      int y = (matrix.height() - 8) / 2; // center the text vertically
 
-    while ( x + width - spacer >= 0 && letter >= 0 ) {
-      if ( letter < messagesOneline.length() ) {
-        matrix.drawChar(x, y, messagesOneline[letter], HIGH, LOW, 1);
+      while ( x + width - spacer >= 0 && letter >= 0 ) {
+        if ( letter < messagesOneline.length() ) {
+          matrix.drawChar(x, y, messagesOneline[letter], HIGH, LOW, 1);
+        }
+
+        letter--;
+        x -= width;
       }
 
-      letter--;
-      x -= width;
+      matrix.write(); // Send bitmap to display
+      delay(wait);
+
     }
-
-    matrix.write(); // Send bitmap to display
-
-    delay(wait);
-  }
 }
 
 void showPage() {
@@ -99,11 +105,10 @@ void handlePost() {
   Serial.println("POST request: ");
   messages = server.arg(0);
   messagesOneline = messages;
-  messagesOneline.replace("\n", "   ");
+  messagesOneline.replace("\r\n", "   ");
   Serial.println(messages);
   
-  // save to EEPROM
-
+  // save messages
   showPage();
 }
 
