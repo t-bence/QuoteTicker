5f983211291667246f1bd592621e49a46ebfd96e
t-bence <toth.bence.mihaly@gmail.com>
2020-03-22 18:15:10 +0100

    edited HTML

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ ESP8266WebServer server(80);
 String messages {"Hello vilag"};
 String messagesOneline {""};
 
-String html_start {"<html><head><style>h1 {font-size: 40px; } .u-full-width { width: 100%; box-sizing: border-box; } .button, button, input { display: block; height: 100px; padding: 0 30px; color: #FFF; text-align: center; font-size: 30px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: #33C3F0; border-radius: 4px; border: 1px solid #33C3F0; cursor: pointer; box-sizing: border-box; } textarea { font-size: 38px; background-color: #fff; border: 1px solid #D1D1D1; border-radius: 4px; box-shadow: none; box-sizing: border-box; } textarea { -webkit-appearance: none; -moz-appearance: none; appearance: none; } textarea { min-height: 65px; padding-top: 6px; padding-bottom: 6px; } textarea:focus { border: 1px solid #33C3F0; outline: 0; }</style><title>QuoteTicker</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\"><script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script></head><body><h1>Add meg az id&eacute;zetet</h1><form action=\"/\" method=\"POST\"><textarea class=\"u-full-width\" id=\"exampleMessage\" name=\"message\" rows=6>"};
+String html_start {"<html><head><style>h1 {font-size: 55px; } .u-full-width { width: 100%; box-sizing: border-box; } .button, button, input { display: block; height: 200px; padding: 0 30px; color: #FFF; text-align: center; font-size: 50px; font-weight: 600; line-height: 38px; letter-spacing: .1rem; text-transform: uppercase; text-decoration: none; white-space: nowrap; background-color: #33C3F0; border-radius: 4px; border: 1px solid #33C3F0; cursor: pointer; box-sizing: border-box; } textarea { font-size: 38px; background-color: #fff; border: 1px solid #D1D1D1; border-radius: 4px; box-shadow: none; box-sizing: border-box; } textarea { -webkit-appearance: none; -moz-appearance: none; appearance: none; } textarea { min-height: 65px; padding-top: 6px; padding-bottom: 6px; } textarea:focus { border: 1px solid #33C3F0; outline: 0; }</style><title>QuoteTicker</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\"><script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script></head><body><h1>Add meg az id&eacute;zetet</h1><form action=\"/\" method=\"POST\"><textarea class=\"u-full-width\" id=\"exampleMessage\" name=\"message\" rows=6>"};
 String html_end {"</textarea><input class=\"button-primary\" type=\"submit\" value=\"K&uuml;ld&eacute;s!\"></form></body></html>"};
 
 void showPage();
@@ -57,7 +57,7 @@ void setup() {
   Serial.println("HTTP server started");
 
   // set up LED display
-  matrix.setIntensity(3);
+  matrix.setIntensity(2);
   
   matrix.setRotation(0, 1);
   matrix.setRotation(1, 1);
@@ -70,7 +70,7 @@ void loop() {
   // display message on the LEDs
     for ( uint16_t i = 0 ; i < width * messagesOneline.length() + matrix.width() - 1 - spacer; i++ ) {
       server.handleClient();
-
+      
       matrix.fillScreen(LOW);
 
       uint16_t letter = i / width;
@@ -95,7 +95,7 @@ void loop() {
 void showPage() {
   // display current settings on webpage
   Serial.println("Showing page");
-  //server.send(200, "text/html", html_start + messages + html_end);
+  server.send(200, "text/html", html_start + messages + html_end);
 
 }
 
